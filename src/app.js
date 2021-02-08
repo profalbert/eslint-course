da80d8da44cdc25efb79698a4eed558ae020c9bd
Albert <alb.adm.ru@gmail.com>
2021-02-08 22:20:15 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,73 @@
+// export default class Post {
+//   constructor(title, img) {
+//     this.title = title;
+//     this.date = new Date(
+
+//     );
+//     this.img = img;
+//   }
+
+//   toString() {
+//     return JSON.stringify(
+//       {
+//         title: this.title,
+//         date: this.date.toString(),
+//         img: this.img,
+//       },
+//       null,
+//       2,
+//     );
+//   }
+
+//   get upperCaseTitle() {
+//     return this.title.toUpperCase()
+//   }
+// }
+
+const foo = 2;
+const fo4o = 24;
+let tuu = 55;
+tuu = 66;
+
+console.log(foo, fo4o, tuu);
+
+
+const rut = {
+  name: 'albert',
+}
+const newRut = { ...rut }
+newRut.name = 'new'
+console.log(rut.name, newRut.name)
+
+// export class Post {
+//   constructor(title, img) {
+//     this.title =
+//     title;
+//     this.date = new Date(
+
+//     );
+//     this.img = img
+//   }
+
+//   toString ( ) {
+//     return JSON.stringify(
+//       {
+//       title: this.title,
+//       date: this.date.toString(),
+//       img: this.img,
+//     },
+//     null,
+//     2);
+//    }
+
+//   get  upperCaseTitle() {
+//     return this.title.toUpperCase();
+//   }
+// }
+
+// var foo = 2;
+// const fo4o = 24;
+// var tuu = 55
+// tuu = 66
+
+// console.log(foo, fo4o);
