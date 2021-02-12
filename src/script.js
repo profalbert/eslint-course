77199b3a5018d63d59df8d3f291e33ebae2044b0
Albert <alb.adm.ru@gmail.com>
2021-02-13 00:10:44 +0300

    add console.log

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const getUser = async () => {
     const res = await fetch('vk.com')
     const data = await res.json()
     console.log(data)
+    console.log(data)
   } catch (e) {
     console.log(e)
   }
