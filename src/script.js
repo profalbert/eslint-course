d8a8d784aa935135968e35bfc6f55683148b4356
Albert <alb.adm.ru@gmail.com>
2021-02-13 00:20:18 +0300

    remove console.log

--- src/script.js
+++ src/script.js
@@ -3,7 +3,6 @@ const getUser = async () => {
     const res = await fetch('vk.com')
     const data = await res.json()
     console.log(data)
-    console.log(data)
   } catch (e) {
     console.log(e)
   }
